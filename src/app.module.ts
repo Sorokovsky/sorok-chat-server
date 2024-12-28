aeedb2b97b57641792e346b230702f5465a58f7b
Sorokovsky <Sorokovskys@ukr.net>
2024-12-28 14:59:40 +0200

    logging start

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
-import { Module } from "@nestjs/common";
+import { ConsoleLogger, Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [ConfigModule.forRoot()],
+  providers: [ConsoleLogger],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { INestApplication } from "@nestjs/common";
+import { ConsoleLogger, INestApplication } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
+import { DEFAULT_PORT } from "./constant/default.constant";
 
 async function bootstrap(): Promise<void> {
   const app: INestApplication = await NestFactory.create(AppModule);
+  app.useLogger(new ConsoleLogger());
   const configService: ConfigService = app.get(ConfigService);
-  const port: number = await configService.get("PORT");
-  await app.listen(port ?? 3000);
+  const logger: ConsoleLogger = app.get(ConsoleLogger);
+  const port: number = (await configService.get("PORT")) ?? DEFAULT_PORT;
+  await app.listen(port);
+  logger.log(`Server is running on port ${port}`, "Server");
 }
 
 bootstrap();
