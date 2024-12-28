b180089167b4c1df58c421b7fe515247808ea40e
Sorokovsky <Sorokovskys@ukr.net>
2024-12-28 14:43:19 +0200

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { INestApplication } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { INestApplication } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap(): Promise<void> {
   const app: INestApplication = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService: ConfigService = app.get(ConfigService);
+  const port: number = await configService.get("PORT");
+  await app.listen(port ?? 3000);
 }
+
 bootstrap();
