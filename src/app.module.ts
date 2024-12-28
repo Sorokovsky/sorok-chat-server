f6e82cfc39d083a1e15da6ee122336ac6175fd6c
Sorokovsky <Sorokovskys@ukr.net>
2024-12-28 22:24:11 +0200

    serve media files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,16 @@ import { ConsoleLogger, Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { getTypeOrmConfig } from "./configs/typeorm.config";
-import { FilesModule } from './files/files.module';
+import { FilesModule } from "./files/files.module";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { SERVER_FOLDER } from "./constants/default.constant";
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: SERVER_FOLDER,
+    }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
