6837a289b8f01943e1f17e96fe10bf6965e42c8a
Sorokovsky <Sorokovskys@ukr.net>
2024-12-31 13:07:44 +0200

    formatting

--- src/core/contracts/dto/auth/login.dto.ts
+++ src/core/contracts/dto/auth/login.dto.ts
@@ -1,6 +1,12 @@
 import { IsEmail, IsString, MaxLength, MinLength } from "class-validator";
-import { MAX_PASSWORD_LENGTH, MIN_PASSWORD_LENGTH } from "@constants/default.constant";
-import { MAX_PASSWORD_MESSAGE, MIN_PASSWORD_MESSAGE } from "@constants/messages.constant";
+import {
+  MAX_PASSWORD_LENGTH,
+  MIN_PASSWORD_LENGTH,
+} from "@constants/default.constant";
+import {
+  MAX_PASSWORD_MESSAGE,
+  MIN_PASSWORD_MESSAGE,
+} from "@constants/messages.constant";
 import { ApiProperty } from "@nestjs/swagger";
 
 export class LoginDto {
