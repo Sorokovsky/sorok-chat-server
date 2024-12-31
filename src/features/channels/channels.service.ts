1734829ffabdacf7cc478c9bdbe7cf663fd2bbc1
Sorokovsky <Sorokovskys@ukr.net>
2024-12-31 20:07:39 +0200

    refactor

--- src/features/channels/channels.service.ts
+++ src/features/channels/channels.service.ts
@@ -3,10 +3,14 @@ import { GetChannelDto } from "@contracts/dto/channel/get-channel.dto";
 import { InjectRepository } from "@nestjs/typeorm";
 import { ChannelEntity } from "@entities/channel.entity";
 import { Repository } from "typeorm";
-import { CreateChannelDtoWithoutFiles } from "@contracts/dto/channel/create-channel.dto";
+import {
+  CreateChannelDto,
+  CreateChannelDtoWithoutFiles,
+} from "@contracts/dto/channel/create-channel.dto";
 import { FilesService } from "@features/files/files.service";
 import { join } from "node:path";
 import { UserEntity } from "@entities/user.entity";
+import { ChannelNotFoundException } from "@exceptions/channel/channel-not-found.exception";
 
 @Injectable()
 export class ChannelsService {
@@ -15,6 +19,20 @@ export class ChannelsService {
     private readonly repository: Repository<ChannelEntity>,
     private readonly filesService: FilesService,
   ) {}
+
+  public async getById(
+    id: number,
+    withError: boolean = false,
+  ): Promise<GetChannelDto> {
+    const channel: GetChannelDto = await this.repository.findOne({
+      where: { id },
+      loadEagerRelations: true,
+    });
+    if (channel === null && withError) {
+      throw new ChannelNotFoundException("id", id);
+    }
+    return channel;
+  }
 
   public async getChannels(userId: number): Promise<GetChannelDto[]> {
     return (await this.repository.find({
@@ -33,22 +51,14 @@ export class ChannelsService {
     image?: Express.Multer.File,
   ): Promise<GetChannelDto> {
     let newChannel: ChannelEntity = await this.repository
-      .create({
-        ...channel,
-      })
+      .create(channel)
       .save();
-    if (avatar) {
-      newChannel.avatarPath = await this.uploadAvatar(
-        avatar,
-        `${newChannel.id}`,
-      );
-    }
-    if (image) {
-      newChannel.imagePath = await this.uploadBackground(
-        image,
-        `${newChannel.id}`,
-      );
-    }
+    const withFiles: ChannelEntity = await this.processFiles(
+      newChannel.id,
+      avatar,
+      image,
+    );
+    newChannel = this.repository.merge(newChannel, withFiles);
     newChannel = await this.repository.save(newChannel);
     return await this.connectUser(userId, newChannel.id);
   }
@@ -57,17 +67,10 @@ export class ChannelsService {
     userId: number,
     channelId: number,
   ): Promise<GetChannelDto> {
-    const channel: ChannelEntity = await this.repository.findOne({
-      where: {
-        id: channelId,
-      },
-    });
+    const channel: GetChannelDto = await this.getById(channelId, true);
     channel.members.push({ id: userId } as UserEntity);
     await this.repository.save(channel);
-    return await this.repository.findOne({
-      where: { id: channelId },
-      loadEagerRelations: true,
-    });
+    return await this.getById(channelId, true);
   }
 
   private async uploadAvatar(
@@ -92,5 +95,20 @@ export class ChannelsService {
       "background",
       true,
     );
+  }
+
+  private async processFiles(
+    id: number,
+    avatar?: Express.Multer.File,
+    image?: Express.Multer.File,
+  ): Promise<ChannelEntity> {
+    const result: ChannelEntity = {} as ChannelEntity;
+    if (avatar) {
+      result.avatarPath = await this.uploadAvatar(avatar, `${id}`);
+    }
+    if (image) {
+      result.imagePath = await this.uploadBackground(image, `${id}`);
+    }
+    return result;
   }
 }
