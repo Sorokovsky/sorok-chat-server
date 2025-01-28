a71750b2d0e0d276a9d0b8868d2c7eb865ba9732
Sorokovsky <Sorokovskys@ukr.net>
2025-01-28 15:47:36 +0200

    feat: create user controller

---
+++ SorokChatServer.API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SorokChatServer.Core.Contracts;
+using SorokChatServer.Core.Interfaces;
+
+namespace SorokChatServer.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IUsersService _usersService;
+
+    public UsersController(IUsersService usersService)
+    {
+        _usersService = usersService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateUserRequest newUser, CancellationToken cancellationToken)
+    {
+        var createdResult = await _usersService.Create(newUser, cancellationToken);
+        if (createdResult.IsFailure)
+        {
+            var error = createdResult.Error;
+            return StatusCode((int)error.StatusCode, error.Message);
+        }
+
+        const string uri = "/users/created";
+        return Created(uri, createdResult.Value);
+    }
+}
