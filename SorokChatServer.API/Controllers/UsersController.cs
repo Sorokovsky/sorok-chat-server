278857a606ee31a5da9ad7d4bd4eb05ff38aaeb1
Sorokovsky <Sorokovskys@ukr.net>
2025-01-29 09:42:22 +0200

    feat: controller user get by id

--- SorokChatServer.API/Controllers/UsersController.cs
+++ SorokChatServer.API/Controllers/UsersController.cs
@@ -22,7 +22,7 @@ public class UsersController : ControllerBase
         if (createdResult.IsFailure)
         {
             var error = createdResult.Error;
-            return StatusCode((int)error.StatusCode, error.Message);
+            return StatusCode((int)error.StatusCode, error);
         }
 
         const string uri = "/users/created";
@@ -38,5 +38,28 @@ public class UsersController : ControllerBase
             user.AvatarPath
         );
         return Created(uri, response);
+    }
+
+    [HttpGet("{id:long}")]
+    public async Task<IActionResult> GetById([FromRoute] long id, CancellationToken cancellationToken)
+    {
+        var userResult = await _usersService.GetById(id, cancellationToken);
+        if (userResult.IsFailure)
+        {
+            var error = userResult.Error;
+            return StatusCode((int)error.StatusCode, error);
+        }
+
+        var user = userResult.Value;
+        return Ok(new UserResponse(
+            user.Id,
+            user.CreatedAt,
+            user.UpdatedAt,
+            user.Email.Value,
+            user.Surname,
+            user.Name,
+            user.MiddleName,
+            user.AvatarPath
+        ));
     }
 }
