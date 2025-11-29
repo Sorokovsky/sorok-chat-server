86c1743f579b8dde58b4fda255c187242c98c24f
Sorokovsky <Sorokovskys@ukr.net>
2025-11-29 21:59:02 +0200

    RSA signing

--- SorokChatServer.Logic/Hubs/IChatsHub.cs
+++ SorokChatServer.Logic/Hubs/IChatsHub.cs
@@ -7,7 +7,7 @@ public interface IChatsHub
     public Task ReceiveMessageAsync(GetMessage message, long chatId);
 
     public Task ReceiveExchangeAsync(string staticPublicKey, string signingStatic, string ephemeralKey,
-        string signingEphemeral, GetUser userId, long chatId);
+        string signingEphemeral, GetUser user, long chatId);
 
     public Task ConnectedAsync(long chatId, GetUser user);
 }
