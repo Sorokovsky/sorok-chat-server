8084c7814b40f54f4f786b9952afd418d4f0e53f
Sorokovsky <Sorokovskys@ukr.net>
2025-12-08 21:00:04 +0200

    refactor

---
+++ SorokChatServer.Mapper/MapperConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace SorokChatServer.Mapper;
+
+public record MapperConfiguration(IEnumerable<TypeMappingConfiguration> TypeMappings);
+
+public record TypeMappingConfiguration(
+    Type SourceType,
+    Type DestType,
+    IEnumerable<MemberMappingConfiguration> MemberMappingConfigurations);
+
+public record MemberMappingConfiguration(string MemberName, IMappingAction MappingAction);
+
+public interface IMappingAction
+{
+}
